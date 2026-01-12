cb867079b2423a6911fdfa5eec3d26edcc7f99c8
Harsh Raj <khushboo1992kumari14@gmail.com>
2026-01-12 16:16:36 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,64 @@
+game.splash("CAR RACING", "Developed By Harsh Raj")
+scene.setBackgroundColor(15)
+let Lane = sprites.create(img`
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    . . . . . . . . 1 . . . . . . . 
+    `, SpriteKind.Player)
+Lane.setPosition(80, 60)
+Lane.setScale(8, ScaleAnchor.Middle)
+let PlayerCar1 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . 1 . . . . 2 2 2 2 2 2 2 2 . . 
+    . . . . . 2 c 2 2 2 2 2 2 4 2 . 
+    . . . . 2 c c 2 2 2 2 2 2 4 c 2 
+    . . d 2 4 c c 2 4 4 4 4 4 4 c c 
+    . d 2 2 4 c b e e e e e e e 2 c 
+    . 2 2 2 4 b e e b b b e b b e 2 
+    . 2 2 2 2 2 e b b b b e b b b e 
+    . 2 2 2 2 e 2 2 2 2 2 e 2 2 2 e 
+    . 2 d d 2 e f e e e f e e e e e 
+    . d d 2 e e e f e e f e e e e e 
+    . e e e e e e e f f f e e e e e 
+    . e e e e f f f e e e e f f f f 
+    . . . e f f f f f e e f f f f f 
+    . . . . f f f f . . . . f f f . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+PlayerCar1.setPosition(40, 100)
+let PlayerCar2 = sprites.create(img`
+    . . . . . . . 8 8 8 8 8 . . . . 
+    . . . . . 8 8 6 6 6 6 6 8 . . . 
+    . . . . 8 8 6 6 6 6 6 6 6 8 . . 
+    . . . . 8 9 7 7 7 6 6 6 7 b 8 . 
+    . . 8 8 9 9 7 7 7 7 6 6 7 9 b 8 
+    . 8 6 6 9 9 7 7 7 7 7 6 7 9 9 8 
+    8 6 6 6 9 9 6 7 7 7 7 7 6 9 9 8 
+    8 6 6 6 9 9 8 8 8 8 8 8 8 9 9 8 
+    8 6 6 6 9 b 8 b b b 8 b 8 b 9 8 
+    8 6 8 8 8 8 b b b b 8 b b 8 b 8 
+    8 8 3 3 8 8 6 6 6 6 8 6 6 8 8 8 
+    8 3 3 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 f f f 8 8 8 8 f f f 8 8 
+    . 8 8 f f f b f 8 8 f f f b f . 
+    . . . . c b b . . . . c b b . . 
+    `, SpriteKind.Player)
+PlayerCar2.setPosition(120, 100)
+let P1Speed = 0
+let P2Speed = 0
+controller.moveSprite(PlayerCar1, 0, 20)
+controller.moveSprite(PlayerCar2, 0, 20)
